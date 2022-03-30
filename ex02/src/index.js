83a70fa5a6280b3a1b72571591ee1f9c28bf8ae5
melikarameli <melikahatunic2001@gmail.com>
2022-03-30 14:30:11 +0200

    Added file

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //Only change code below this line
+
+    //Only change code above this line
+}
+
+console.log(main());
+//First comment
+module.exports = main;
+
+/*This is a comment
+in more than one line*/
