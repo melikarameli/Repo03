c21b4654d5b1c299c192771dcfad04e83fe183fd
melikarameli <melikahatunic2001@gmail.com>
2022-03-30 15:17:00 +0200

    Added ex07

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,8 +1,8 @@
 function main() {
-    //Only change code below this line
+    // Only change code below this line
     x = 10;
     y = x;
-    //Only change code above this line
+    // Only change code above this line
     return [x, y];
 }
 
