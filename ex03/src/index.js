36d21d4df7adab19bee98dfadd44a0a0d074ad44
melikarameli <melikahatunic2001@gmail.com>
2022-03-31 09:24:51 +0200

    Added

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,6 +1,6 @@
 function main() {
     // Only change code below this line
-
+    var forCat;
     // Only change code above this line
 }
 
@@ -8,4 +8,3 @@ console.log(main());
 
 module.exports = main;
 
-var forCat;
